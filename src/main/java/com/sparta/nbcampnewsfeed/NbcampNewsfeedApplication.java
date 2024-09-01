3aa22509d0c3d240d5be3556566ddbbbcbb1ef6b
inseooo0 <calmandpassion@korea.ac.kr>
2024-09-01 21:25:49 +0900

    initial commit

---
+++ src/main/java/com/sparta/nbcampnewsfeed/NbcampNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcampnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampNewsfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbcampNewsfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/nbcampnewsfeed/NbcampNewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcampnewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampNewsfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
