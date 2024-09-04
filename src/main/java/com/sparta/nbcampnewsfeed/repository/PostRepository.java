19b7cddcefc14035ad64b02d858c7d426178a856
inseooo0 <calmandpassion@korea.ac.kr>
2024-09-04 15:06:02 +0900

    feat: 회원 탈퇴시 작성한 게시물, 댓글 전체 삭제

--- src/main/java/com/sparta/nbcampnewsfeed/repository/PostRepository.java
+++ src/main/java/com/sparta/nbcampnewsfeed/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.nbcampnewsfeed.repository;
 
 import com.sparta.nbcampnewsfeed.entity.Post;
+import com.sparta.nbcampnewsfeed.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
     // 친구들의 게시물을 가져오기 위한 쿼리 (내림차순 정렬)
     Page<Post> findAllByUserUserIdInOrderByCreatedAtDesc(List<Long> userIds, Pageable pageable);
+
+    List<Post> findAllByUser(User user);
 }
