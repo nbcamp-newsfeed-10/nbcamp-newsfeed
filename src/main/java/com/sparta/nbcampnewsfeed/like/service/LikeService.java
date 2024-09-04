55a258d10c17354081df52d690c6cc9d1ba8a9e5
inseooo0 <calmandpassion@korea.ac.kr>
2024-09-04 22:43:31 +0900

    feat: like service 예외 처리 추가

--- src/main/java/com/sparta/nbcampnewsfeed/like/service/LikeService.java
+++ src/main/java/com/sparta/nbcampnewsfeed/like/service/LikeService.java
@@ -1,6 +1,8 @@
 package com.sparta.nbcampnewsfeed.like.service;
 
+import com.sparta.nbcampnewsfeed.ApiPayload.Code.Status.ErrorStatus;
 import com.sparta.nbcampnewsfeed.auth.dto.requestDto.AuthUser;
+import com.sparta.nbcampnewsfeed.exception.ApiException;
 import com.sparta.nbcampnewsfeed.like.entity.Like;
 import com.sparta.nbcampnewsfeed.post.entity.Post;
 import com.sparta.nbcampnewsfeed.profile.entity.User;
@@ -22,17 +24,17 @@ public class LikeService {
 
     @Transactional
     public LikePostResponseDto likePost(Long postId, AuthUser authUser) {
-        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Invalid post ID"));
-        User user = userRepository.findById(authUser.getId()).orElseThrow(()-> new IllegalArgumentException("Invalid user"));
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ApiException(ErrorStatus._NOT_FOUND_POST));
+        User user = userRepository.findById(authUser.getId()).orElseThrow(()-> new ApiException(ErrorStatus._NOT_FOUND_USER));
 
         // 자신이 작성한 게시물에 좋아요를 누를 수 없음
         if (post.getUser().getUserId().equals(user.getUserId())) { //로그인한 유저의 아이디와 게시물 작성한 유저의 아이디를 비교하는 것
-            throw new IllegalArgumentException("You cannot like your own post");
+            throw new ApiException(ErrorStatus._BAD_REQUEST_SELF_LIKE);
         }
 
         // 이미 좋아요를 눌렀는지 확인 //데이터베이스에 포스트아이디랑 회원아이디가 있는지 확인해서 있다면 좋아요를 눌렀다는 증거!
         if (likeRepository.findByPostAndUser(post, user).isPresent()) {
-            throw new IllegalArgumentException("You have already liked this post");
+            throw new ApiException(ErrorStatus._BAD_REQUEST_DOUBLE_LIKE);
         }
 
         // 좋아요 추가
@@ -44,11 +46,11 @@ public class LikeService {
 
     @Transactional
     public void unlikePost(Long postId, AuthUser authUser) {
-        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Invalid post ID"));
-        User user = userRepository.findById(authUser.getId()).orElseThrow(()-> new IllegalArgumentException("Invalid user"));
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ApiException(ErrorStatus._NOT_FOUND_POST));
+        User user = userRepository.findById(authUser.getId()).orElseThrow(()-> new ApiException(ErrorStatus._NOT_FOUND_USER));
 
         // 좋아요 기록이 있는지 확인
-        Like like = likeRepository.findByPostAndUser(post, user).orElseThrow(() -> new IllegalArgumentException("You haven't liked this post yet"));
+        Like like = likeRepository.findByPostAndUser(post, user).orElseThrow(() -> new ApiException(ErrorStatus._NOT_FOUND_LIKE));
 
         // 좋아요 취소
         likeRepository.delete(like);
