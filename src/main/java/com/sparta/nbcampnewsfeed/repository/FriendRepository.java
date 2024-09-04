39e17122ee71933f40b9053e29c69386a4e176e2
inseooo0 <calmandpassion@korea.ac.kr>
2024-09-04 14:38:07 +0900

    feat: 친구 요청 api 예외 처리 추가

--- src/main/java/com/sparta/nbcampnewsfeed/repository/FriendRepository.java
+++ src/main/java/com/sparta/nbcampnewsfeed/repository/FriendRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface FriendRepository extends JpaRepository<Friend, Long> {
     Optional<Friend> findByToUserAndFromUser(User toUser, User fromUser);
 
-    Optional<Friend> findByToUserAndFromUserAndAndFriendStatus(User toUser, User fromUser, boolean friendStatus);
+    Optional<Friend> findByToUserAndFromUserAndFriendStatus(User toUser, User fromUser, boolean friendStatus);
 
     // 내가 받은 친구 요청 중 친구 상태가 true인 경우 (toUser가 본인인 경우)
     List<Friend> findAllByToUserAndFriendStatus(User toUser, boolean friendStatus);
