062b834f4e641865a71dfbc213997d89e23d6515
vege4944 <vege4944@gmail.com>
2024-09-05 11:52:50 +0900

    feat: like count get

--- src/main/java/com/sparta/nbcampnewsfeed/like/repository/LikeRepository.java
+++ src/main/java/com/sparta/nbcampnewsfeed/like/repository/LikeRepository.java
@@ -9,5 +9,9 @@ import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByPostAndUser (Post post, User user);
+
+    // 특정 게시물의 좋아요 개수를 카운트하는 메서드
+    // 좋아요 개수는 0아니면 정수니까 null 이 들어올 수 있는 가능성이 없어서 optional 안넣어도됨
+    Long countByPost(Post post);
 }
 
